b3fdc36533d8f09aa4fa07d19a0e217f2ef56d90
LunaLaTuna <jikooklifu9597@gmail.com>
2026-05-04 23:25:31 -0600

    Cambios para R2

--- cotizador/settings.py
+++ cotizador/settings.py
@@ -140,11 +140,11 @@ if USE_ENVIRONMENT:
     # Usar R2 como almacenamiento por defecto
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = 'https://pub-e12a508ebf5b4627bbe7b95f18c0a9f5.r2.dev'
-    MEDIA_ROOT = BASE_DIR / 'media'
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_ENDPOINT_URL = 'https://1fb3617d540a1715fa0e7ee4e34dcec1.r2.cloudflarestorage.com'
+    AWS_S3_CUSTOM_DOMAIN =os.getenv('AWS_S3_CUSTOM_DOMAIN')
 
 
     STATIC_URL = '/static/'
